5a0af729480b93624213ca28ff9c4960a40c5aee
jaceyh <jacey.hallock@gmail.com>
2023-12-26 15:03:20 -0500

    add repo to github

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+//import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// import screens
+import Start from './components/Start';
+import Chat from './components/Chat';
+
+// Create the navigator
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+    return (
+        <NavigationContainer>
+            
+            <Stack.Navigator
+            initialRouteName="Start" >
+                <Stack.Screen
+                name="Start"
+                component={Start}
+                />
+                <Stack.Screen
+                name="Chat"
+                component={Chat}
+                />
+            </Stack.Navigator>
+
+        </NavigationContainer>
+   );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +40,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
---
+++ components/Start.js
@@ -0,0 +1,113 @@
+import { useState } from 'react';
+import { ImageBackground, StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import Chat from './Chat';
+
+const image = {uri: '../assets/BackgroundImage.png'}
+
+const Start = ({ navigation }) => {
+    const [ name, setName ] = useState('');
+    const [ background, setBackground ] = useState('white');
+    const colors = [ '#090C08', '#474056', '#8A95A5', '#B9C6AE'];
+
+    return (
+        //background image full screen -- also parent componenet
+        <ImageBackground 
+        source={require("../assets/BackgroundImage.png")} 
+        style={styles.container}>
+            <Text style={styles.title}>Chat App</Text>
+            <View style={styles.box}>
+            <TextInput
+            style={styles.textInput}
+            value={name}
+            onChangeText={setName}
+            inlineImageLeft='../assets/icon.svg'
+            placeholder='Type your username here.'
+            />
+            <Text style={styles.text}>Choose your background color:</Text>
+            <View style={styles.colorList}>
+                {colors.map((color, index) => (
+                    <TouchableOpacity 
+                    key={index}
+                    style={[styles.circle, { backgroundColor: color }, background === color && styles.selectedColor,]} onPress={() => setBackground(color)} />
+                ))}
+        </View>  
+            <TouchableOpacity
+            style={styles.button}
+            title="Enter Chat"
+            onPress={() => navigation.navigate('Chat', {name: name, color: background})}>
+            <Text
+            style={styles.buttonText}>Enter Chat</Text>
+            </TouchableOpacity>
+            </View>
+        </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        fontColor: '#fff',
+        paddingBottom: 15
+    },
+    box: {
+        display: 'block',        
+        width: '88%',
+        height: '44%',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        backgroundColor: '#ffffff',
+    },
+    button: {
+        width: '88%',
+        margin: 15,
+        padding: 10,
+        alignItems: 'center',
+        backgroundColor: '#757083'
+    },
+    buttonText: {
+        fontSize: 16,
+        fontWeight: '600',
+        color: '#FFFFFF',
+    },
+    circle: {
+        width: 50,
+        height: 50,
+        margin: 10,
+        borderRadius: 25,
+    },
+    colorList: {
+        flexDirection: 'row',
+    },
+    selectedColor: {
+        padding: 10,
+        borderWidth: 3,
+        borderColor: '#757083',
+    },
+    text: {
+        fontSize: 16,
+        fontWeight: '300',
+        color: '#757083'
+    },
+    textInput: {
+        fontSize: 16,
+        fontWeight: '300',
+        color: '#757083',
+        opacity: 50,
+        width: '88%',
+        padding: 10,
+        borderWidth: 1.5,
+        borderColor: '#757083',
+        marginTop: 10,
+        marginBottom: 10
+    },
+    title: {
+        fontSize: 45,
+        fontWeight: '600',
+        color: '#ffffff',
+        paddingTop: 50,
+    },
+});
+
+export default Start;
