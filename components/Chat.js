cbafdd3704983403aafe625dfef3200a15c13da5
jaceyh <jacey.hallock@gmail.com>
2023-12-27 12:01:56 -0500

    import GiftedChat and declare messages state

--- components/Chat.js
+++ components/Chat.js
@@ -1,7 +1,10 @@
-import { useEffect } from 'react';
+import { GiftedChat } from "react-native-gifted-chat";
+import { useState, useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
 const Chat = ({ route, navigation }) => {
+
+    const [messages, setMessages] = useState([]);
 
     const { name } = route.params;
     const { color } = route.params;
@@ -9,6 +12,21 @@ const Chat = ({ route, navigation }) => {
     useEffect(() => {
         navigation.setOptions({title: name});
     }, []);
+
+    useEffect(() => {
+        setMessages([
+          {
+            _id: 1,
+            text: "Hello developer",
+            createdAt: new Date(),
+            user: {
+              _id: 2,
+              name: "React Native",
+              avatar: "https://placeimg.com/140/140/any",
+            },
+          },
+        ]);
+      }, []);
 
 
     return (
